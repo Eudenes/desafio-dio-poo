99f3724b80c19e7838f5708965c3b195fead06ea
Eudenes <eng.eudenes@gmail.com>
2022-03-08 17:11:00 -0300

    BootCamp e DEvs Create

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import com.dio.desafios.Curso;
-import com.dio.desafios.Mentoria;
+import com.dio.desafios.*;
 
 import java.time.LocalDate;
 
@@ -22,11 +21,49 @@ public class Main {
         mentoria.setDescricao("descição mentoria java");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("BootCamp Java Dev");
+        bootcamp.setDescricao("Descrição BootCamp Java Dev");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Dev dev1= new Dev();
+        dev1.setNome("Eudenes");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: "+ dev1.getConteudosInscritos());
+        System.out.println(" Xp : " + dev1.calcularTotalXp());
+        dev1.progredir();
+        System.out.println("Conteudos inscritos: "+ dev1.getConteudosInscritos());
+        System.out.println("Conteudos concluidos: "+ dev1.getConteudosConcluidos());
+        System.out.println(" Xp : " + dev1.calcularTotalXp());
 
+        System.out.println(" -----------");
+
+        Dev dev2= new Dev();
+        dev2.setNome("Mayra");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: "+ dev2.getConteudosInscritos());
+        System.out.println(" Xp : " + dev2.calcularTotalXp());
+        dev2.progredir();
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("Conteudos inscritos: "+ dev2.getConteudosInscritos());
+        System.out.println("Conteudos concluidos: "+ dev2.getConteudosConcluidos());
+        System.out.println(" Xp : " + dev2.calcularTotalXp());
+
+//        System.out.println(bootcamp);
+//        System.out.println(dev1);
+//        System.out.println(dev2);
+
+
+
+
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(conteudo);
+//        System.out.println(mentoria);
+//
 
     }
 }
